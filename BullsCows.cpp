f6a949c6b5c25144323a1e60d58ab85b361feeb6
Gabriela Hołowacz <57449657+gholowacz@users.noreply.github.com>
2019-12-12 22:36:31 +0100

    Add files via upload
    Update 12.12 22.36

--- BullsCows.cpp
+++ BullsCows.cpp
@@ -14,14 +14,14 @@ bool AskToPlayAgain();
 
 
 // the entry point for our application
-
 int main()
 {
-	PrintIntro();
-	
-	PlayGame();
-
-	AskToPlayAgain(); 
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
 
 	return 0; // exit the application 
 }
@@ -60,13 +60,16 @@ string GetGuess()
 	getline(cin, Guess);
 	return Guess; 
 }
+
 bool AskToPlayAgain()
 {
-	cout << "Do you want to play again?";
+	cout << "Do you want to play again? (y/n)";
 	string Response = "";
 	getline(cin, Response);
-	return (Response[0] == 'y') || (Response [0] == 'Y');
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
+
+
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
